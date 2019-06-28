db4149dc085d14ad9433bd1135f12c53a2356771
Trung Nguyen <43191329+kylenguyen1014@users.noreply.github.com>
2019-06-27 22:19:04 -0400

    initialize starwars_api_app (react)

---
+++ src/People.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function People({name, height, gender}) {
+    return (
+        <div>
+            <h2>Name</h2>
+            <p>Height</p>
+            <p>Gender</p>
+        </div>
+    )
+}
+
+export default People;
